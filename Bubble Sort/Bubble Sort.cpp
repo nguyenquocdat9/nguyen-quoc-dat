8e2e0958256de5c50346a717b1126ffe92c4366e
nguyenquocdat9 <124764098+nguyenquocdat9@users.noreply.github.com>
2023-09-08 01:25:01 +0700

    Add files via upload

---
+++ Bubble Sort/Bubble Sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0;i < n;i++) {
+        cin >> arr[i];
+    }
+    for(int i = 0;i < n;i++) {
+        bool swapped = false;
+        for(int j = 0;j < n - 1;j++) {
+            if(arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if(swapped == false) break;
+    }
+    for(int i = 0;i < n;i++) {
+        cout << arr[i] << " ";
+    }
+}
